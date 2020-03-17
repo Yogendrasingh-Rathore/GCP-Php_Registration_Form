b669b344da47736818f6a85fe33b49cb74b1aacf
Yogendrasingh-Rathore <53259324+Yogendrasingh-Rathore@users.noreply.github.com>
2020-03-17 19:04:34 +0530

    Delete sql_cloud_env.sh

--- Env/sql_cloud_env.sh
+++
@@ -1,6 +0,0 @@
-export SQL_INSTANCE=Php-sql
-export SQL_INSTANCE_REGION=us-central1
-export SQL_INSTANCE_ZONE=us-central1-a
-export SQL_DATA_DISK=10
-export SQL_ROOT_PASSWORD=jdks6577sakjd
-echo "Exported Env Variables"
